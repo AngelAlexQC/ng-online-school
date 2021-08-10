c3f8d4bd238bddc51dbfcd5ae6731ef02e27df7a
Ángel Quiroz <guirudj007@gmailc.om>
2021-08-09 23:10:03 -0500

    Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ContactPageComponent } from "./pages/contact-page/contact-page.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { IndexPageComponent } from "./pages/index-page/index-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: IndexPageComponent,
+  },
+  {
+    path: "home",
+    component: HomePageComponent,
+  },
+  {
+    path: "contact",
+    component: ContactPageComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./components/layout/navbar/navbar.component";
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { IndexPageComponent } from './pages/index-page/index-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, HomePageComponent, IndexPageComponent, ContactPageComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
